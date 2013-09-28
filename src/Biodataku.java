25a00bf3348bf8a18f66e6c6cf9461296a498c52
unknown <dede.rahman7291@gmail.com>
2013-09-28 16:54:04 +0700

    commit03

---
+++ src/Biodataku.java
@@ -0,0 +1,13 @@
+//Perintah penggunaan package versi 01
+package Latihan01.sesi1.bin;
+
+public class Biodataku
+{
+	public static void main(String[] data)
+	{
+		System.out.println("N a m a	        : Dede Abdurahman");
+		System.out.println("Alamat  	    : Depok");
+		System.out.println("Golongan darah	: A");
+		System.out.println("Jenis Kelamin	: Laki-laki");
+	}
+}
