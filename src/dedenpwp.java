7b814bf525e3f6ea5c07be0aae2a03fde3ded53e
unknown <dede.rahman7291@gmail.com>
2013-09-28 17:52:11 +0700

    commit07

---
+++ src/dedenpwp.java
@@ -0,0 +1,13 @@
+//Perintah penggunaan package NPWP
+package Latihan02.sesi1.bin;
+
+public class dedenpwp
+{
+	public static void main(String[] data)
+	{
+		System.out.println("N P W P        : 09.254.294.3-407.000");
+		System.out.println("N a m a        : Dede Abdurahman");
+		System.out.println("Alamat         : Depok");
+		System.out.println("Tgl Terdaftar  : 01-10-2010");
+	}
+}
