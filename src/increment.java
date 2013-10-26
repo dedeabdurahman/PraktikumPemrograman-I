81cc48a9ea1991ea44eca230f2876477262cfa57
unknown <dede.rahman7291@gmail.com>
2013-10-26 22:46:06 +0700

    commit08

---
+++ src/increment.java
@@ -0,0 +1,32 @@
+/*
+*Class Increment
+*/ 
+package Latihan01.sesi1.bin;
+
+public class operator
+{
+public static void main(String[] args)
+{
+int a;
+a=120;
+System.out.println ("contoh postfix increment : ");
+System.out.println (a);
+System.out.println (a++);
+System.out.println (a);
+a=120;
+System.out.println ("Contoh prefix increment : ");
+System.out.println (a);
+System.out.println (++a);
+System.out.println (a);
+a=120;
+System.out.println ("Contoh Postfix decrement : ");
+System.out.println (a);
+System.out.println (a--);
+System.out.println(a);
+a=120;
+System.out.println ("Contoh Prefix decrement : ");
+System.out.println (a);
+System.out.println (--a);
+System.out.println (a);
+}
+}
