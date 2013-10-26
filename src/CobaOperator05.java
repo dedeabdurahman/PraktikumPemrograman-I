e46d74529c4f82362b878794e112d844bbfbc91e
unknown <dede.rahman7291@gmail.com>
2013-10-26 22:54:32 +0700

    commit12

---
+++ src/CobaOperator05.java
@@ -0,0 +1,35 @@
+//Perintah Operator
+package Latihan01.sesi1.bin;
+
+public class CobaOperator05
+{
+public static void main(String[] Xx )
+{
+int c=3; int d=5; int e=4; float f=6; int g=12;
+System.out.println("Nilai awal c adalah " +c);
+c+=7;
+System.out.println("Nilai c+=7 adalah " +c);
+System.out.println();
+System.out.println();
+System.out.println("Nilai awal d adalah " +d);
+d-=4;
+System.out.println("Nilai d-=4 adalah " +d);
+System.out.println();
+System.out.println();
+System.out.println("Nilai awal e adalah " +e);
+e*=5;
+System.out.println("Nilai e*=5 adalah " +e);
+System.out.println();
+System.out.println();
+System.out.println("Nilai awal f adalah " +f);
+f/=5;
+System.out.println("Nilai f/=5 adalah " +f);
+System.out.println();
+System.out.println();
+System.out.println("Nilai awal g adalah " +g);
+g%=7;
+System.out.println("Nilai g%=7 adalah " +g);
+System.out.println();
+System.out.println();
+}
+}
